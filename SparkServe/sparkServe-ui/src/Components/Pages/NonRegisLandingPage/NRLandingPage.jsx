c9fc2f4048cc87ab2d872bb16bb9afdb236e3d3e
Morgan-AV <mvill235@fiu.edu>
2024-07-16 15:51:16 -0700

    Creation of grid and cards on NRLanding

--- SparkServe/sparkServe-ui/src/Components/Pages/NonRegisLandingPage/NRLandingPage.jsx
+++ SparkServe/sparkServe-ui/src/Components/Pages/NonRegisLandingPage/NRLandingPage.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Navbar from '../../Navbar/Navbar'
 import { Container, Grid } from '@mui/material'
 import Footer from '../../Footer/Footer'
-
+import Cards from '../../Cards/Cards'
+import DummyData from '../../DumyData/DummyData'
 
 const NRLandingPage = () => {
     return (
@@ -12,8 +13,25 @@ const NRLandingPage = () => {
                 <div style={{ padding: '20px', background: 'lightblue' }}>
                     <p>Hello World</p>
                 </div>
-                <Grid>
+                <Grid 
+                    container rowSpacing={4} 
+                    columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
                     Many cards
+                    {posts.map((post) => (
+                        <Grid item xs={12} sm={6} md={4} key={post.id}>
+                            <Cards 
+                                title={post.title}
+                                cover={post.cover}
+                                author={post.author}
+                                view={post.view}
+                                comment={post.comment}
+                            />
+                        </Grid>
+                    ))}
                 </Grid>
             </Container>
             <Footer/>
