e6c24e14633e4be7bc58d4a9edc6a4775eb8eecc
Ahmedh27 <ahmed2hamouda2@.com>
2024-07-18 14:23:38 -0700

    Routed Sign-In/Reister Button Using clerk API

--- SparkServe/sparkServe-ui/src/Components/UserNavBar/UserNavBar.jsx
+++ SparkServe/sparkServe-ui/src/Components/UserNavBar/UserNavBar.jsx
@@ -8,7 +8,8 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Drawer from '@mui/material/Drawer';
 import MenuItem from '@mui/material/MenuItem';
 import logo2 from '../../assets/logo2.png';
-
+import UserRegistration from "../Pages/UserRegistration/UserRegistration";
+ 
 const logoStyle = {
   width: '140px',
   height: 'auto',
@@ -45,12 +46,7 @@ function UserNavBar() {
             <img src={logo2} style={logoStyle} alt="Logo" />
           </Box>
           <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1, alignItems: 'center' }}>
-            <Button color="primary" variant="text" size="small" href="/signin">
-              Sign in
-            </Button>
-            <Button color="primary" variant="contained" size="small" href="/signup">
-              Sign up
-            </Button>
+            <UserRegistration />
           </Box>
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
             <Button variant="text" color="primary" onClick={toggleDrawer(true)}>
@@ -59,14 +55,7 @@ function UserNavBar() {
             <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
               <Box sx={{ width: 250, p: 2 }}>
                 <MenuItem>
-                  <Button color="primary" variant="contained" href="/signup" sx={{ width: '100%' }}>
-                    Sign up
-                  </Button>
-                </MenuItem>
-                <MenuItem>
-                  <Button color="primary" variant="outlined" href="/signin" sx={{ width: '100%' }}>
-                    Sign in
-                  </Button>
+                  <UserRegistration />
                 </MenuItem>
               </Box>
             </Drawer>
