a65dac2c19a431d02b2f294993b88e98da3e8409
larnelle15 <larnelleankunda50@gmail.com>
2024-07-22 15:28:13 -0700

    Changed folders names

---
+++ SparkServe/sparkServe-ui/src/Components/Pages/UserSignUpPage/UserSignUpPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserSignUp from '../../UserSignUp/UserSignUp';
+
+const UserSignUpPage = () => {
+  return (
+    <UserSignUp />
+  );
+};
+
+export default UserSignUpPage;
