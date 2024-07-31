d4ade4d56d1481227736446e5b177d58dd0cffb0
Morgan-AV <mvill235@fiu.edu>
2024-07-31 16:34:29 -0700

    Back navigation for sign up pages

--- SparkServe/sparkServe-ui/src/Components/Pages/UserSignUpPage/UserSignUpPage.jsx
+++ SparkServe/sparkServe-ui/src/Components/Pages/UserSignUpPage/UserSignUpPage.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import UserSignUp from '../../UserSignUp/UserSignUp';
+import { IconButton } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const UserSignUpPage = () => {
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  const navigate = useNavigate();
+
+
   return (
+    <>
+    
+    <IconButton onClick={handleGoBack} aria-label="go back" sx={{ mt: 8, ml: 8 }}>
+      <ArrowBack />
+    </IconButton>
     <UserSignUp />
+
+    </>
   );
 };
 
